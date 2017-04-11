4c0b2da1f3f646ccdaf00d49e57bc83e08ec7a36
John David Back <john@casamatic.com>
2017-04-11 13:25:57 -0400

    Adding conditional symbolic link creation

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
-sudo ln -s /opt/apps/jdb/nginx.conf /etc/nginx/sites-enabled/jdb
+if [ ! -f /etc/nginx/sites-enabled/jdb ]; then
+  sudo ln -s /opt/apps/jdb/nginx.conf /etc/nginx/sites-enabled/jdb
+fi
 # sudo ln -s /opt/apps/jdb/uwsgi.ini /etc/uwsgi/apps-enabled/jdb.ini
 
 sudo service nginx restart
