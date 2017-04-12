aa1ee2ac0633b3a5e4475c93011f66d674ecbff4
John David Back <john@casamatic.com>
2017-04-11 20:58:25 -0400

    Daemonize into the tmp log

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 
 # uwsgi --ini /opt/apps/jdb/uwsgi.ini -s /opt/apps/jdb/jdb.sock --daemonize /var/log/uwsgi/access
-/usr/local/bin/uwsgi --emperor /etc/uwsgi/sites
+/usr/local/bin/uwsgi --emperor /etc/uwsgi/sites --daemonize /tmp/uwsgi.log
