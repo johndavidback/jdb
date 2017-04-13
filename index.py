f9a193c0b55ac3c36eda6afcdf05c40acd4b79b7
John David Back <john@casamatic.com>
2017-04-12 20:19:27 -0400

    Adding in all the static and media files that we need

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Welcome to hell! Get yourself a glass of water.'
-
-if __name__ == "__main__":
-    app.run()
+  return render_template('index.html')
